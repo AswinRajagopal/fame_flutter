13e494495934db1583d6d64b09413d64fcd585bd
Jay Tarpara <tarparajay00@gmail.com>
2020-11-30 11:32:41 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'views/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(PocketFaME());
+
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
+  await Hive.openBox('fame_pocket');
+}
+
+class PocketFaME extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'FaME',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.ibmPlexSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+      getPages: [
+        GetPage(
+          name: 'login',
+          page: () => LoginPage(),
+        ),
+      ],
+    );
+  }
+}
