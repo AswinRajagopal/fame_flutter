01120ddfa5219dd2747b119b03e42cfb17dfa2f6
Jay Tarpara <tarparajay00@gmail.com>
2021-01-25 10:36:11 +0530

    Changed on tap to date from container

--- lib/widgets/home_calendar.dart
+++ lib/widgets/home_calendar.dart
@@ -123,72 +123,73 @@ class _HomeCalendarState extends State<HomeCalendar> with TickerProviderStateMix
           );
         },
         dayBuilder: (context, date, events) {
-          return InkWell(
-            onTap: () {
-              var showDate = '${date.year}-${date.month <= 9 ? '0' + date.month.toString() : date.month}-${date.day <= 9 ? '0' + date.day.toString() : date.day}';
-              if (calC.calendarType == 'myRos') {
-                if (events != null) {
-                  print(events);
-                  var eveSplit = events.first.split(',');
-                  var eveLength = events.first.split(',').length;
-                  var clSplit = eveSplit.last.split('#');
-                  var client1;
-                  var client2;
-                  if (clSplit.length > 1) {
-                    client1 = clSplit[0];
-                    client2 = clSplit[1];
-                  } else {
-                    client1 = eveSplit.last;
+          return Container(
+            // margin: const EdgeInsets.all(4.0),
+            margin: const EdgeInsets.only(top: 4.0),
+            padding: const EdgeInsets.only(top: 15.0, left: 28.0),
+            width: 100,
+            height: 300,
+            child: GestureDetector(
+              onTap: () {
+                // print('On Date');
+                var showDate = '${date.year}-${date.month <= 9 ? '0' + date.month.toString() : date.month}-${date.day <= 9 ? '0' + date.day.toString() : date.day}';
+                if (calC.calendarType == 'myRos') {
+                  if (events != null) {
+                    print(events);
+                    var eveSplit = events.first.split(',');
+                    var eveLength = events.first.split(',').length;
+                    var clSplit = eveSplit.last.split('#');
+                    var client1;
+                    var client2;
+                    if (clSplit.length > 1) {
+                      client1 = clSplit[0];
+                      client2 = clSplit[1];
+                    } else {
+                      client1 = eveSplit.last;
+                    }
+                    // if (eveLength > 1) {
+                    Get.defaultDialog(
+                      title: 'Roster on $showDate',
+                      content: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            '$client1 : ${eveSplit[0]}',
+                          ),
+                          eveLength > 1 && (eveSplit[1] != null && client2 != null)
+                              ? Text(
+                                  '$client2 : ${eveSplit[1]}',
+                                )
+                              : SizedBox(),
+                        ],
+                      ),
+                      radius: 5.0,
+                    );
+                    // }
                   }
-                  // if (eveLength > 1) {
-                  Get.defaultDialog(
-                    title: 'Roster on $showDate',
-                    content: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          '$client1 : ${eveSplit[0]}',
-                        ),
-                        eveLength > 1 && (eveSplit[1] != null && client2 != null)
-                            ? Text(
-                                '$client2 : ${eveSplit[1]}',
-                              )
-                            : SizedBox(),
-                      ],
-                    ),
-                    radius: 5.0,
-                  );
-                  // }
+                } else {
+                  print('My Calendar');
+                  print(events);
+                  var calEvent = events.first.split('*');
+                  if (calEvent.length > 1) {
+                    Get.defaultDialog(
+                      title: 'Employee Detail on $showDate',
+                      content: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Checked In @ ${calEvent[1].split(',')[0]}',
+                          ),
+                          Text(
+                            'Checked Out @ ${calEvent[1].split(',')[1]}',
+                          ),
+                        ],
+                      ),
+                      radius: 5.0,
+                    );
+                  }
                 }
-              } else {
-                print('My Calendar');
-                print(events);
-                var calEvent = events.first.split('*');
-                if (calEvent.length > 1) {
-                  Get.defaultDialog(
-                    title: 'Employee Detail on $showDate',
-                    content: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Checked In @ ${calEvent[1].split(',')[0]}',
-                        ),
-                        Text(
-                          'Checked Out @ ${calEvent[1].split(',')[1]}',
-                        ),
-                      ],
-                    ),
-                    radius: 5.0,
-                  );
-                }
-              }
-            },
-            child: Container(
-              // margin: const EdgeInsets.all(4.0),
-              margin: const EdgeInsets.only(top: 4.0),
-              padding: const EdgeInsets.only(top: 15.0, left: 28.0),
-              width: 100,
-              height: 300,
+              },
               child: Text(
                 '${date.day}',
                 style: TextStyle().copyWith(fontSize: 16.0),
